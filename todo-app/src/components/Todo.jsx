e0c2586b9c71994e7b036a0d591804b1130231d8
LKW <leekw0228@gamil.com>
2025-06-10 17:21:33 +0900

    d

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -5,32 +5,32 @@ import TopMove from "./TopMove";
 import CheckBox from "./CheckBox";
 
 const Todo = () => {
-    const [todos, setTodos] = useState([]);
-    const inputRef = useRef("");
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef("");
 
-    const addTodo = (e) => {
-        e.preventDefault();
+  const addTodo = (e) => {
+    e.preventDefault();
 
-        const todo = inputRef.current.value;
-        setTodos([...todos, todo]);
+    const todo = inputRef.current.value;
+    setTodos([...todos, todo]);
 
-        inputRef.current.value = "";
-        inputRef.current.focus();
-    };
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
 
-    return (
-        <div className="flex flex-col justify-center items-center">
-            <h1>Todo List</h1>
-            <div>
-                <form className="flex items-center justify-center h-[2.2rem] gap-x-3" onSubmit={addTodo}>
-                    <input type="text" className="h-full m-0 p-0 box-border text-xl px-2  rounded-md" ref={inputRef} />
-                    <input
-                        type="submit"
-                        className="flex justify-center items-center h-full px-3 py-2 rounded-md cursor-pointer border "
-                        value="등록"
-                    />
-                </form>
-            </div>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1>Todo List</h1>
+      <div>
+        <form className="flex items-center justify-center h-[2.2rem] gap-x-3" onSubmit={addTodo}>
+          <input type="text" className="h-full m-0 p-0 box-border text-xl px-2  rounded-md" ref={inputRef} />
+          <input
+            type="submit"
+            className="flex justify-center items-center h-full px-3 py-2 rounded-md cursor-pointer border "
+            value="등록"
+          />
+        </form>
+      </div>
 
       <ol className="w-3/4 space-y-4">
         {todos.map((item, index) => (
